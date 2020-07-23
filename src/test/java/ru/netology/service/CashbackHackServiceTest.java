101a389b137c22a5877823dffd8dbb9969f0bc00
Ivan Shumilov <ivan.shumilov91@gmail.com>
2020-07-23 16:45:54 +0300

    added some more tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -23,4 +23,20 @@ public class CashbackHackServiceTest {
         assertEquals(actual, expected);
     }
 
+    @Test
+    public void checkingZeroBoundary() {
+        int actual = service.remain(0);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void checkingBoundary() {
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
 }
